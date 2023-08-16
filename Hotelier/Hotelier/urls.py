76a6bc72cfe66f5b9f7fa0e75a1b371da42de234
Eniola Hannah <heniolahannah@gmail.com>
2023-08-16 08:45:48 +0100

    I added the url link of booking.html to the url.py and at the href

--- Hotelier/Hotelier/urls.py
+++ Hotelier/Hotelier/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', TemplateView.as_view(template_name = 'index.html'), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('booking/', TemplateView.as_view(template_name='booking.html'), name='booking'),
 
 ]
