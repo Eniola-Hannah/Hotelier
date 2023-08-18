c4d120a243255171b0f2f3699a8089e785dbd476
Eniola Hannah <heniolahannah@gmail.com>
2023-08-18 16:57:16 +0100

    I added the name each app i created the other time belong to

--- Hotelier/Hotelier/paymentApp/apps.py
+++ Hotelier/Hotelier/paymentApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PaymentappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "paymentApp"
+    name = "Hotelier.paymentApp"
--- Hotelier/Hotelier/userApp/views.py
+++ Hotelier/Hotelier/userApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
