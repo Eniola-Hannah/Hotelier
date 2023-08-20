b0c526caef69a32acb24d54bfeca3e81a7606ffa
Eniola Hannah <heniolahannah@gmail.com>
2023-08-20 14:18:13 +0100

    I added code of function to show user's account

--- Hotelier/Hotelier/userApp/views.py
+++ Hotelier/Hotelier/userApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views import generic
 from .forms import SignUpForm
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -10,4 +13,10 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-
+# every function that is coming from urls must take 'request' as the first parameter
+@login_required
+def my_account(request, _id):
+    profile = Profile.objects.all().filter(user_id=_id) 
+    print(profile)
+    # every render must return, add return and request as a parameter
+    return render(request=request, template_name="userApp/my_account.html", context={"my_profile":profile})  
