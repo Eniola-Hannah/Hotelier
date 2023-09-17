323157cd0a718d29dbdcc25982ba0d3865eb81e4
Eniola Hannah <heniolahannah@gmail.com>
2023-09-17 22:14:27 +0100

    I added neccessary function codes in my views.py

--- Hotelier/Hotelier/roomsApp/views.py
+++ Hotelier/Hotelier/roomsApp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponsePermanentRedirect
+from .forms import Rooms_form
+from .models import Room
+from django.urls import reverse
+from django.contrib import messages
+\
 
 # Create your views here.
+@login_required
+def addRoom(request):
+    if request.method == 'POST':
+        room_form = Rooms_form(request.POST, request.FILES)
+        if room_form.is_valid():
+            room_form.save()
+            messages.success(request, ('Room was successfully Created!'))
+            return displayServices(request, "service_admin")
+        else:
+            messages.error(request, ('Room was not created successfully!'))
+            return render(request=request, template_name='roomsApp/add_room.html', context={"roomForm": room_form})
+
+    else:
+        room_form = Rooms_form()
+        return render(request=request, template_name='roomsApp/add_room.html', context={"roomForm": room_form})
