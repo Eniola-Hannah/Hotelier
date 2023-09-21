bf46e9171e7a12a33311a184fe63733275a0c5a3
Eniola Hannah <heniolahannah@gmail.com>
2023-09-21 07:32:15 +0100

    I added series of template for roomsApp

--- Hotelier/Hotelier/roomsApp/urls.py
+++ Hotelier/Hotelier/roomsApp/urls.py
@@ -3,5 +3,6 @@ from Hotelier.roomsApp import views as rv
 
 urlpatterns = [
     re_path(r'^add_room/', rv.addRoom, name="add_room"),
+    re_path(r'^display_rooms/(?P<display>\w+)/', rv.displayRooms, name="display_rooms"),
 
 ]
--- Hotelier/Hotelier/roomsApp/views.py
+++ Hotelier/Hotelier/roomsApp/views.py
@@ -5,7 +5,7 @@ from .forms import Rooms_form
 from .models import Room
 from django.urls import reverse
 from django.contrib import messages
-\
+
 
 # Create your views here.
 @login_required
@@ -15,7 +15,7 @@ def addRoom(request):
         if room_form.is_valid():
             room_form.save()
             messages.success(request, ('Room was successfully Created!'))
-            return displayServices(request, "service_admin")
+            return displayRooms(request, "service_admin")
         else:
             messages.error(request, ('Room was not created successfully!'))
             return render(request=request, template_name='roomsApp/add_room.html', context={"roomForm": room_form})
@@ -23,3 +23,11 @@ def addRoom(request):
     else:
         room_form = Rooms_form()
         return render(request=request, template_name='roomsApp/add_room.html', context={"roomForm": room_form})
+    
+@login_required
+def displayRooms(request, display):
+    rooms = Room.objects.all()
+    if display == "service_nologin":
+        return render(request=request, template_name='roomsApp/rooms.html', context={"rooms": rooms})
+    else:
+        return render(request=request, template_name='roomsApp/display_rooms.html', context={"rooms": rooms})
